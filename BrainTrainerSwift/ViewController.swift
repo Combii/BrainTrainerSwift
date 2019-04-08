a36223fb90114f7c07ced979d8d083ce692fed84
Boris Grunwald <borisgrunwald@gmail.com>
2019-04-08 12:46:12 +0200

    Added action

--- BrainTrainerSwift/ViewController.swift
+++ BrainTrainerSwift/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnClicked(_ sender: UIButton) {
+        
+            print(sender.titleLabel!.text)
+        
+    }
+    
 }
 
