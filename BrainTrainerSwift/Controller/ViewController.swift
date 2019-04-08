7bba4655f33c07245685c0671d6f086f5d7cad5e
Boris Grunwald <borisgrunwald@gmail.com>
2019-04-08 13:20:23 +0200

    Timer almost working

--- BrainTrainerSwift/Controller/ViewController.swift
+++ BrainTrainerSwift/Controller/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
    
-    @IBOutlet weak var timer: UITextField!
+    
+    @IBOutlet weak var timerLabel: UITextField!
+    
     
     @IBOutlet weak var score: UITextField!
+    
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel.text = "\(seconds)" //This will update the label.
+    }
     
     @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
