4b74d8388b5eb299b4eb5f59aa7ce0baa4eb4972
Combii <davidstovle@gmail.com>
2019-04-26 15:23:16 +0200

    Working change view without data

---
+++ BrainTrainerSwift/Controller/DifficultyController.swift
@@ -0,0 +1,9 @@
+//
+//  DifficultyController.swift
+//  BrainTrainerSwift
+//
+//  Created by David Støvlbæk on 26/04/2019.
+//  Copyright © 2019 David Støvlbæk. All rights reserved.
+//
+
+import Foundation
